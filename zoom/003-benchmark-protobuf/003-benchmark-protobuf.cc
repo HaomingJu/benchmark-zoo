b5905b023499080e7672cc1f1a3a7182f71f1bd9
haomingOnDocker <juhaoming@someemail.com>
2024-09-23 17:27:46 +0800

    测试protobuf各种情况下时间消耗, 关键字repeated, arena

--- zoom/003-benchmark-protobuf/003-benchmark-protobuf.cc
+++ zoom/003-benchmark-protobuf/003-benchmark-protobuf.cc
@@ -1,17 +1,56 @@
 #include <benchmark/benchmark.h>
 
-#include <cstdio>          // for printf
-#include <glog/logging.h>  // for glog
-#include <iostream>        // for std::cout
-#include <spdlog/spdlog.h> // for spdlog
+#include <Person.pb.h>
+#include <SearchRequest.pb.h>
 
-static void BM_todo(benchmark::State &state) {
+#include <memory> // for std::make_shared
+
+static void BM_protobuf_obj(benchmark::State &state) {
   for (auto _ : state) {
-      // TODO
+    Person p;
+    p.set_name("ZhangSan");
+    p.set_age(8);
   }
 }
 
+static void BM_protobuf_smart_ptr(benchmark::State &state) {
+  for (auto _ : state) {
+    auto p = std::make_shared<Person>();
+    p->set_name("ZhangSan");
+    p->set_age(8);
+  }
+}
 
-BENCHMARK(BM_todo);
+static void BM_protobuf_obj_repeated(benchmark::State &state) {
+  SearchRequest sr;
+  for (auto _ : state) {
+    sr.add_page_number(32);
+    sr.add_query("GOD IS LOVE");
+  }
+}
+
+static void BM_protobuf_smart_ptr_repeated(benchmark::State &state) {
+  auto srPtr = std::make_shared<SearchRequest>();
+  for (auto _ : state) {
+    srPtr->add_page_number(32);
+    srPtr->add_query("GOD IS LOVE");
+  }
+}
+
+#include <google/protobuf/arena.h>
+static void BM_protobuf_smart_ptr_repeated_arena(benchmark::State &state) {
+  google::protobuf::Arena arena;
+  auto srPtr = google::protobuf::Arena::CreateMessage<SearchRequest>(&arena);
+  for (auto _ : state) {
+    srPtr->add_page_number(32);
+    srPtr->add_query("GOD IS LOVE");
+  }
+}
+
+BENCHMARK(BM_protobuf_obj);
+BENCHMARK(BM_protobuf_smart_ptr);
+BENCHMARK(BM_protobuf_obj_repeated);
+BENCHMARK(BM_protobuf_smart_ptr_repeated);
+BENCHMARK(BM_protobuf_smart_ptr_repeated_arena);
 
 BENCHMARK_MAIN();
